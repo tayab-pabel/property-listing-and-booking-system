6a958542a4a4e46ddc65f4d868fef062f44e11cb
[tayab-pabel] <[tayabpabel@yahoo.com]>
2021-10-29 17:50:26 +0600

    search filter updated

--- front-end/src/Components/Sections/Demo.js
+++ front-end/src/Components/Sections/Demo.js
@@ -82,7 +82,7 @@ const Demo = () => {
             leaveFrom="translate-x-0"
             leaveTo="translate-x-full"
           >
-            <div className="ml-auto relative max-w-xs w-full h-full bg-white shadow-xl py-4 pb-6 flex flex-col overflow-y-auto">
+            <div className="ml-auto relative max-w-full w-full h-full bg-white shadow-xl py-4 pb-6 flex flex-col overflow-y-auto">
               <div className="px-4 flex items-center justify-between">
                 <h2 className="text-lg font-medium text-gray-900">Filters</h2>
                 <button
@@ -144,12 +144,7 @@ const Demo = () => {
       </Transition.Root>
 
       <div className="max-w-3xl mx-auto px-4 text-center sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="py-24">
-          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900">New Arrivals</h1>
-          <p className="mt-4 max-w-3xl mx-auto text-base text-gray-500">
-            Thoughtfully designed objects for the workspace, home, and travel.
-          </p>
-        </div>
+        
 
         <section aria-labelledby="filter-heading" className="border-t border-gray-200 py-6">
           <h2 id="filter-heading" className="sr-only">
