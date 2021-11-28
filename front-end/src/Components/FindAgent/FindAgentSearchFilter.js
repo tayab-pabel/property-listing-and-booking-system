3a3dd789f776fea2212eb3efc0c28d6462cea6b6
[tayab-pabel] <[tayabpabel@yahoo.com]>
2021-11-28 21:44:22 +0600

    Find Agent UI Done

--- front-end/src/Components/FindAgent/FindAgentSearchFilter.js
+++ front-end/src/Components/FindAgent/FindAgentSearchFilter.js
@@ -3,11 +3,11 @@ import React from 'react';
 const FindAgentSearchFilter = () => {
   return (
     <div className="bg-gray-100">
-      <div className="max-w-4xl mx-auto px-4 py-5 sm:py-10">
-        <h2 className="text-3xl leading-6 font-medium text-blue-dark">Find an Agent</h2>
+      <div className="max-w-4xl mx-auto px-4 py-10 sm:py-10">
+        <h2 className="text-3xl leading-6 font-bold text-blue-dark">Find an Agent</h2>
         <form className="mt-5 sm:flex sm:items-center">
-          <div className="w-full">
-            <label 
+          <div className="w-full mt-5 sm:mt-0">
+            <label
               htmlFor="location" 
               className="block text-left text-sm font-medium text-blue-dark"
             >
@@ -22,7 +22,7 @@ const FindAgentSearchFilter = () => {
             />
           </div>
 
-          <div className="w-full sm:mt-0 sm:ml-3">
+          <div className="w-full mt-5 sm:mt-0 sm:ml-3">
             <label 
               htmlFor="name" 
               className="block text-left text-sm font-medium text-blue-dark"
@@ -38,32 +38,32 @@ const FindAgentSearchFilter = () => {
             />
           </div>
 
-          <div className='w-full sm:mt-0 sm:ml-3'>
-                    <label
-                      htmlFor='radius'
-                      className='block text-left text-sm font-medium text-blue-dark'
-                    >
-                      Radius
-                    </label>
-                    <select
-                      id='searchRadius'
-                      name='searchRadius'
-                      className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-blue-dark focus:outline-none focus:ring-blue-light focus:border-blue-light text-blue-dark placeholder-blue-dark sm:text-sm rounded-lg'
-                      defaultValue='Rent'
-                    >
-                      <option value='0'>This area only</option>
-                      <option value='0.25'>+ ¼ mile</option>
-                      <option value='0.5'>+ ½ mile</option>
-                      <option value='1'>+ 1 mile</option>
-                      <option value='3'>+ 3 miles</option>
-                      <option value='5'>+ 5 miles</option>
-                      <option value='10'>+ 10 miles</option>
-                      <option value='15'>+ 15 miles</option>
-                      <option value='20'>+ 20 miles</option>
-                      <option value='30'>+ 30 miles</option>
-                      <option value='40'>+ 40 miles</option>
-                    </select>
-                  </div>                  
+          <div className='w-full mt-5 sm:mt-0 sm:ml-3'>
+            <label
+              htmlFor='radius'
+              className='block text-left text-sm font-medium text-blue-dark'
+            >
+              Radius
+            </label>
+            <select
+              id='searchRadius'
+              name='searchRadius'
+              className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-blue-dark focus:outline-none focus:ring-blue-light focus:border-blue-light text-blue-dark placeholder-blue-dark sm:text-sm rounded-lg'
+              defaultValue='Rent'
+            >
+              <option value='0'>This area only</option>
+              <option value='0.25'>+ ¼ mile</option>
+              <option value='0.5'>+ ½ mile</option>
+              <option value='1'>+ 1 mile</option>
+              <option value='3'>+ 3 miles</option>
+              <option value='5'>+ 5 miles</option>
+              <option value='10'>+ 10 miles</option>
+              <option value='15'>+ 15 miles</option>
+              <option value='20'>+ 20 miles</option>
+              <option value='30'>+ 30 miles</option>
+              <option value='40'>+ 40 miles</option>
+            </select>
+          </div>                  
 
           <button
             type="submit"
