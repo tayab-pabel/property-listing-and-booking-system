7e5497c445ebca0ef2b97a3f00cdf4d3069c4f79
aminruhul14 <ramin5849@gmail.com>
2021-12-11 00:39:14 +0600

    Property Detail Component Done

--- client/src/Components/FindProperty/PropertyDetails.js
+++ client/src/Components/FindProperty/PropertyDetails.js
@@ -4,7 +4,7 @@ import Footer from '../Sections/Footer';
 import Header from '../Sections/Header';
 import ImageGallery from 'react-image-gallery';
 import { Link } from 'react-router-dom';
-import { ArrowLeftIcon, HeartIcon, PhoneIcon, ShareIcon } from '@heroicons/react/outline';
+import { ArrowLeftIcon, CheckCircleIcon, CheckIcon, HeartIcon, PhoneIcon, ShareIcon } from '@heroicons/react/outline';
 import { Dialog, Transition } from '@headlessui/react';
 
 const PropertyDetails = () => {
@@ -135,7 +135,7 @@ const PropertyDetails = () => {
     businessAndCommunication: ['Intercom', 'Broadband', 'Satellite or Cable TV'],
     security: ['Security Alarm System', 'Electronic Security', 'CCTV Camera'],
     descriptions:
-      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, maiores!',
+      "Recognize the best when you’re in Dhanmondi. Filled with restaurants, banks, shopping centers and recreational zones, the area is complete with all the amenities one could ask for. People from various places in the city come to the area on weekends. It makes sense since there’s so much to do here. Built in great design with a spectacular outlook, this apartment boasts much more than meets the eye. With creatively structured rooms and intelligent architectural cuts, residents will know what it's like to live in pride. There are three bedrooms here and two of them have washrooms attached. One of them has a balcony attached. You also get separate spaces to be used as the dining and the drawing areas. There is also an attendant's bedroom and bathroom available within the abode. ",
     agentInfo: {
       name: 'Benham and Reeves',
       phoneNumber: '+44 20 3463 7502',
@@ -281,13 +281,97 @@ const PropertyDetails = () => {
                 </div>
 
                 <div className='mt-10'>
+                  <h3 className='text-lg text-blue-dark font-bold'>Floor Plan</h3>
+                  <div>
+                    <div 
+                      onClick={openModal}
+                      className="mt-2 p-5 rounded-lg max-w-sm mx-auto border border-blue-light shadow cursor-pointer">
+                      <img 
+                        className="w-80 mx-auto"
+                        src={property[0].floorPlanImage} 
+                        alt="" 
+                      />
+                      <button
+                        type="button"
+                        
+                        className="px-4 mt-4 py-2 mx-auto flex text-sm font-medium text-white bg-blue-light rounded-lg"
+                      >
+                        <svg className="h-5 w-5 mr-1.5" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20" data-testid="floorplans-icon"><path fill-rule="evenodd" d="M1.875.125a1.75 1.75 0 00-1.75 1.75v16.25c0 .967.784 1.75 1.75 1.75h16.25a1.75 1.75 0 001.75-1.75v-6.517a1.75 1.75 0 00-1.75-1.75h-7.283V7.24a.75.75 0 00-1.5 0v2.62h-2.33a.75.75 0 000 1.5h2.33v.531l-1.607 1.62A.75.75 0 008.8 14.567l1.829-1.844a.749.749 0 00.213-.605v-.76h7.283a.25.25 0 01.25.25v6.517a.25.25 0 01-.25.25h-7.3c.011-.082.017-.165.017-.25V16.13a.75.75 0 00-1.5 0v1.995a.25.25 0 01-.25.25H1.875a.25.25 0 01-.25-.25v-6.517a.25.25 0 01.25-.25h1.318a.75.75 0 000-1.5H1.875c-.085 0-.168.006-.25.018V1.875a.25.25 0 01.25-.25h7.217a.25.25 0 01.25.25v1.276l-1.607 1.62A.75.75 0 008.8 5.828l1.671-1.685a.75.75 0 00.37-.593.752.752 0 00.001-.171V1.875a1.75 1.75 0 00-1.75-1.75H1.875z" clip-rule="evenodd"></path></svg>
+                        See Floor Plan
+                      </button>
+                    </div>
+
+                    <Transition appear show={isOpen} as={Fragment}>
+                      <Dialog
+                        as="div"
+                        className="fixed inset-0 z-10 bg-gray-900 bg-opacity-80 transition-opacity"
+                        onClose={closeModal}
+                      >
+                        <div className="min-h-screen px-4 text-center">
+                          <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Dialog.Overlay className="fixed inset-0" />
+                          </Transition.Child>
+
+                          {/* This element is to trick the browser into centering the modal contents. */}
+                          <span
+                            className="inline-block h-screen align-middle"
+                            aria-hidden="true"
+                          >
+                            &#8203;
+                          </span>
+                          <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                          >
+                            <div className="inline-block max-w-xl p-10 overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-lg">
+                              
+                              <div className="">
+                                <img 
+                                  className="h-full w-full"
+                                  src={property[0].floorPlanImage} 
+                                  alt="" 
+                                />
+                              </div>
+
+                              <div className="mt-4 max-w-full mx-auto">
+                                <button
+                                  type="button"
+                                  className="px-4 py-2 text-sm font-medium text-white bg-blue-light rounded-lg"
+                                  onClick={closeModal}
+                                >
+                                  Close
+                                </button>
+                              </div>
+                            </div>
+                          </Transition.Child>
+                        </div>
+                      </Dialog>
+                    </Transition>
+                  </div>
+                </div>
+
+                <div className='mt-10'>
                   <h3 className='text-lg text-blue-dark font-bold'>Features & Amenities</h3>
-                  <div className='grid grid-cols-4 gap-4 text-base text-blue-dark'>
+                  <div className='grid grid-cols-2 text-base text-blue-dark mt-3'>
                   {property[0].featuresAndAmenities.map((feature, index) => (
                     <div
-                    className=""
+                    className="flex text-base items-center"
                      key={index}
                     >
+                      <CheckCircleIcon className="w-5 h-5 text-blue-light mr-1.5"></CheckCircleIcon>
                       {feature}
                     </div>
                   ))}
@@ -295,86 +379,13 @@ const PropertyDetails = () => {
                 </div>
 
                 <div className='mt-10'>
-                  <h3 className='text-lg text-blue-dark font-bold'>Features & Amenities</h3>
+                  <h3 className='text-lg text-blue-dark font-bold'>Description</h3>
+                  <p className='text-base text-justify text-blue-dark mt-3'>
+                    {property[0].descriptions}
+                  </p>
                 </div>
+                
 
-                <br />
-                <br />
-                <br />
-                <br />
-                <div>
-                  <div className="">
-                    <button
-                      type="button"
-                      onClick={openModal}
-                      className="px-4 py-2 flex text-sm font-medium text-white bg-blue-light rounded-lg"
-                    >
-                      <svg className="h-5 w-5 mr-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 20 20" data-testid="floorplans-icon"><path fill-rule="evenodd" d="M1.875.125a1.75 1.75 0 00-1.75 1.75v16.25c0 .967.784 1.75 1.75 1.75h16.25a1.75 1.75 0 001.75-1.75v-6.517a1.75 1.75 0 00-1.75-1.75h-7.283V7.24a.75.75 0 00-1.5 0v2.62h-2.33a.75.75 0 000 1.5h2.33v.531l-1.607 1.62A.75.75 0 008.8 14.567l1.829-1.844a.749.749 0 00.213-.605v-.76h7.283a.25.25 0 01.25.25v6.517a.25.25 0 01-.25.25h-7.3c.011-.082.017-.165.017-.25V16.13a.75.75 0 00-1.5 0v1.995a.25.25 0 01-.25.25H1.875a.25.25 0 01-.25-.25v-6.517a.25.25 0 01.25-.25h1.318a.75.75 0 000-1.5H1.875c-.085 0-.168.006-.25.018V1.875a.25.25 0 01.25-.25h7.217a.25.25 0 01.25.25v1.276l-1.607 1.62A.75.75 0 008.8 5.828l1.671-1.685a.75.75 0 00.37-.593.752.752 0 00.001-.171V1.875a1.75 1.75 0 00-1.75-1.75H1.875z" clip-rule="evenodd"></path></svg>
-                      Floor Plan
-                    </button>
-                  </div>
-
-                  <Transition appear show={isOpen} as={Fragment}>
-                    <Dialog
-                      as="div"
-                      className="fixed inset-0 z-10 bg-gray-900 bg-opacity-80 transition-opacity"
-                      onClose={closeModal}
-                    >
-                      <div className="min-h-screen px-4 text-center">
-                        <Transition.Child
-                          as={Fragment}
-                          enter="ease-out duration-300"
-                          enterFrom="opacity-0"
-                          enterTo="opacity-100"
-                          leave="ease-in duration-200"
-                          leaveFrom="opacity-100"
-                          leaveTo="opacity-0"
-                        >
-                          <Dialog.Overlay className="fixed inset-0" />
-                        </Transition.Child>
-
-                        {/* This element is to trick the browser into centering the modal contents. */}
-                        <span
-                          className="inline-block h-screen align-middle"
-                          aria-hidden="true"
-                        >
-                          &#8203;
-                        </span>
-                        <Transition.Child
-                          as={Fragment}
-                          enter="ease-out duration-300"
-                          enterFrom="opacity-0 scale-95"
-                          enterTo="opacity-100 scale-100"
-                          leave="ease-in duration-200"
-                          leaveFrom="opacity-100 scale-100"
-                          leaveTo="opacity-0 scale-95"
-                        >
-                          <div className="inline-block max-w-xl p-10 overflow-hidden align-middle transition-all transform bg-white shadow-xl rounded-lg">
-                            
-                            <div className="">
-                              <img 
-                                className="h-full w-full"
-                                src={property[0].floorPlanImage} 
-                                alt="" 
-                              />
-                            </div>
-
-                            <div className="mt-4 max-w-full mx-auto">
-                              <button
-                                type="button"
-                                className="px-4 py-2 text-sm font-medium text-white bg-blue-light rounded-lg"
-                                onClick={closeModal}
-                              >
-                                Close
-                              </button>
-                            </div>
-                          </div>
-                        </Transition.Child>
-                      </div>
-                    </Dialog>
-                  </Transition>
-
-                </div>
               </div>
               <div className="lg:col-span-4">
                 <div className="p-5 mb-5 bg-white border border-blue-dark rounded-lg shadow">
