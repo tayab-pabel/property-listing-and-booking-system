db941c5b68754b75cbe26e980e4f7c7feda3432c
pronazmul <developernazmul@gmail.com>
2021-12-29 01:06:45 +0600

    localStorage added Successfully

--- client/src/utilities/localStorage.js
+++ client/src/utilities/localStorage.js
@@ -1,13 +1,22 @@
+/*
+ * Add new object to localStorage as array
+ * Overwrite is current one is existed
+ */
 export const propertySavetoLocalStorage = (property) => {
-  let savedProperties = JSON.parse(localStorage.getItem('saveProperties'))
-  let existProperty = savedProperties.find((x) => {
-    x._id === property._id
-  })
-
-  if (existProperty) {
-    newItem = [...existProperty, property]
+  let savedProperties = JSON.parse(localStorage.getItem('saveProperties')) || []
+  if (savedProperties.length > 0) {
+    let propertyExist = savedProperties.find((i) => i._id === property._id)
+    if (propertyExist) {
+      let newProperty = savedProperties.map((i) =>
+        i._id === property._id ? property : i
+      )
+      localStorage.setItem('saveProperties', JSON.stringify(newProperty))
+    } else {
+      savedProperties.push(property)
+      localStorage.setItem('saveProperties', JSON.stringify(savedProperties))
+    }
   } else {
-    newItem = [property]
+    savedProperties.push(property)
+    localStorage.setItem('saveProperties', JSON.stringify(savedProperties))
   }
-  localStorage.setItem('saveProperties', JSON.stringify(newItem))
 }
